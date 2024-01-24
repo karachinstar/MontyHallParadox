5dda177faeaa8dc95c6d63034a46eeb3bfa14332
Семён <karachinstar@yandex.ru>
2024-01-25 00:27:23 +0800

    k

--- src/main/java/org/example/MontyHallParadox.java
+++ src/main/java/org/example/MontyHallParadox.java
@@ -16,18 +16,20 @@ import java.util.Random;
  * Вывести на экран статистику по победам и поражениям.
  */
 
+
 public class MontyHallParadox {
+
     static Random random;
-    static Map<Integer, Boolean> results1;       // Статистика для игрока, не меняющего свой выбор.
-    static Map<Integer, Boolean> results2;       // Статистика для игрока, изменяющего свой выбор.
-    static int doorsNumber;                      // Количество дверей.
+    static Map<Integer, Boolean> staticNotChains;       // Статистика для игрока, не меняющего свой выбор.
+    static Map<Integer, Boolean> staticChains;       // Статистика для игрока, изменяющего свой выбор.
+    static int doorsNub;                      // Количество дверей.
     static int attempts;                         // Количество попыток.
 
     public static void main(String[] args) {
         random = new Random();
-        results1 = new HashMap<>();
-        results2 = new HashMap<>();
-        doorsNumber = 3;
+        staticNotChains = new HashMap<>();
+        staticChains = new HashMap<>();
+        doorsNub = 3;
         attempts = 1000;
 
         for (int i = 0; i < attempts; i++) {     // Розыгрыш (1000 попыток).
@@ -35,7 +37,7 @@ public class MontyHallParadox {
         }
 
         int win = 0;                             // Статистика для первого игрока, не меняющего свой выбор.
-        for (Map.Entry<Integer, Boolean> entry: results1.entrySet()){
+        for (Map.Entry<Integer, Boolean> entry: staticNotChains.entrySet()){
             if (entry.getValue()){
                 win++;
             }
@@ -45,7 +47,7 @@ public class MontyHallParadox {
         System.out.println("Количество побед: " + win + " раз из " + attempts + " попыток.");
 
         win = 0;                                  // Статистика для второго игрока, изменяющего свой выбор.
-        for (Map.Entry<Integer, Boolean> entry: results2.entrySet()){
+        for (Map.Entry<Integer, Boolean> entry: staticChains.entrySet()){
             if (entry.getValue()){
                 win++;
             }
@@ -54,30 +56,33 @@ public class MontyHallParadox {
         System.out.println("Количество побед: " + win + " раз из " + attempts + " попыток.");
     }
 
+
     private static void trial(int numRound) {
-        int success = random.nextInt(doorsNumber);
-        int firstChoice = random.nextInt(doorsNumber);
+        int success = random.nextInt(doorsNub);
+        int firstChoice = random.nextInt(doorsNub);
         int freeOpenDoor = -1;
         int secondChoice = -1;
 
-        for (int i = 0; i < doorsNumber; i++) {
+        for (int i = 0; i < doorsNub; i++) {
             if (i != success && i != firstChoice){
                 freeOpenDoor = i;
             }
         }
 
-        for (int i = 0; i < doorsNumber; i++) {            // Игрок не изменяет свой выбор.
-            if (i != freeOpenDoor && i != firstChoice){
-                secondChoice = firstChoice;
-            }
-        }
-        results1.put(numRound, success == secondChoice);   // Статистика для первого игрока.
 
-        for (int i = 0; i < doorsNumber; i++) {            // Игрок не изменяет свой выбор.
+
+        for (int i = 0; i < doorsNub; i++) {            // Игрок не изменяет свой выбор.
             if (i != freeOpenDoor && i != firstChoice){
                 secondChoice = i;
             }
         }
-        results2.put(numRound, success == secondChoice);   // Статистика для второго игрока.
+        staticChains.put(numRound, success == secondChoice);   // Статистика для второго игрока.
+
+        for (int i = 0; i < doorsNub; i++) {            // Игрок не изменяет свой выбор.
+            if (i != freeOpenDoor && i != firstChoice){
+                secondChoice = firstChoice;
+            }
+        }
+        staticNotChains.put(numRound, success == secondChoice);   // Статистика для первого игрока.
     }
 }
